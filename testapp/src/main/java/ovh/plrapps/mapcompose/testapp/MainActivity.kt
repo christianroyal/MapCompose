9852833c30c862994a5d0df9be05b619fb541089
plr <plr.devs@gmail.com>
2022-01-18 11:43:07 +0100

    Anomaly in layout size

--- testapp/src/main/java/ovh/plrapps/mapcompose/testapp/MainActivity.kt
+++ testapp/src/main/java/ovh/plrapps/mapcompose/testapp/MainActivity.kt
@@ -2,19 +2,15 @@ package ovh.plrapps.mapcompose.testapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.ComposeView
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
-import ovh.plrapps.mapcompose.api.addLayer
-import ovh.plrapps.mapcompose.api.enableRotation
-import ovh.plrapps.mapcompose.api.scrollTo
-import ovh.plrapps.mapcompose.api.shouldLoopScale
-import ovh.plrapps.mapcompose.core.TileStreamProvider
-import ovh.plrapps.mapcompose.testapp.core.ui.MapComposeTestApp
-import ovh.plrapps.mapcompose.testapp.core.ui.theme.MapComposeTheme
-import ovh.plrapps.mapcompose.ui.MapUI
-import ovh.plrapps.mapcompose.ui.state.MapState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +18,29 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<ComposeView>(R.id.map).setContent {
-            val tileStreamProvider = TileStreamProvider { row, col, _ ->
-                applicationContext.assets?.open("tiles/test/tile_0_${col}_$row.png")
+            var text by remember {
+                mutableStateOf("Hello")
             }
-            val state = MapState(4, 4096, 4096).apply {
-                addLayer(tileStreamProvider)
+
+            Layout(
+                content = { Text(text) },
+                modifier = Modifier.clickable {
+                    text += '!'
+                }
+            ) { measurables, constraints ->
+                val placeables = measurables.map { measurable ->
+                    // Measure each children
+                    measurable.measure(constraints)
+                }
+
+                println("max dim (${constraints.maxWidth}, ${constraints.maxHeight})")
+
+                layout(constraints.maxWidth, constraints.maxHeight) {
+                    placeables.forEach { placeable ->
+                        placeable.place(x = 0, y = 0)
+                    }
+                }
             }
-            MapUI(state = state)
         }
     }
 }
