bd870234b4f474612fb2f54fb54ea836fe30ee52
Nohus <git@nohus.eu>
2022-01-18 02:43:00 +0000

    ComposeView bug

--- testapp/src/main/java/ovh/plrapps/mapcompose/testapp/MainActivity.kt
+++ testapp/src/main/java/ovh/plrapps/mapcompose/testapp/MainActivity.kt
@@ -3,16 +3,32 @@ package ovh.plrapps.mapcompose.testapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ovh.plrapps.mapcompose.api.addLayer
+import ovh.plrapps.mapcompose.api.enableRotation
+import ovh.plrapps.mapcompose.api.scrollTo
+import ovh.plrapps.mapcompose.api.shouldLoopScale
+import ovh.plrapps.mapcompose.core.TileStreamProvider
 import ovh.plrapps.mapcompose.testapp.core.ui.MapComposeTestApp
 import ovh.plrapps.mapcompose.testapp.core.ui.theme.MapComposeTheme
+import ovh.plrapps.mapcompose.ui.MapUI
+import ovh.plrapps.mapcompose.ui.state.MapState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MapComposeTheme {
-                MapComposeTestApp()
+        setContentView(R.layout.activity_main)
+
+        findViewById<ComposeView>(R.id.map).setContent {
+            val tileStreamProvider = TileStreamProvider { row, col, _ ->
+                applicationContext.assets?.open("tiles/test/tile_0_${col}_$row.png")
             }
+            val state = MapState(4, 4096, 4096).apply {
+                addLayer(tileStreamProvider)
+            }
+            MapUI(state = state)
         }
     }
 }
